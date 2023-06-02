3746fc38d8256497031617a8931177299978881a
CaoThang <thangly2k1@gmail.com>
2023-06-02 08:50:24 +0700

    ts-array

---
+++ 05-array/array.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255],
+    []
+];
+superHeros.push("spiderman ");
+heroPower.push(2);
+allUsers.push({ name: 'caothang', isActive: true });
